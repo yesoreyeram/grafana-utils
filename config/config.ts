2b80e41d97010c13ddf57c7bdb9acae813712703
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2020-08-28 10:54:46 +0100

    Randomwalk test case added

--- config/config.ts
+++ config/config.ts
@@ -4,6 +4,8 @@ const args = parse(Deno.args);
 
 export const PORT = args.port || 8080;
 
+export const NAMESPACE = `GrafanaDenoAPIServer`;
+
 export const BANNER = `Simple Grafana Deno REST API Server.
 
 Refer https://github.com/yesoreyeram/grafana-simple-deno-api-backend for more details.`;
---
+++ data/utils.test.ts
@@ -0,0 +1,17 @@
+import { assertEquals } from "./../test_deps.ts";
+import { getRandomWalkDataPoints } from "./utils.ts";
+import { NAMESPACE } from "./../config/config.ts";
+
+Deno.test({
+  name: `${NAMESPACE}.Mock.RandomWalk`,
+  fn: () => {
+    const randomWalk = getRandomWalkDataPoints(
+      new Date("2020-12-20 10:20:0").getTime(),
+      new Date("2020-12-20 12:20:0").getTime(),
+    );
+    assertEquals(randomWalk.length, 120);
+    assertEquals(randomWalk[0].length, 2);
+    assertEquals(randomWalk[0][1], 1608459600000);
+    assertEquals(typeof randomWalk[0][0], "number");
+  },
+});
