187e19ab637ba4ce7751b10c23bc8726cbb7ee28
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2020-08-29 20:49:21 +0100

    Testing vercel

--- mod.ts
+++ mod.ts
@@ -9,5 +9,7 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(NOTFOUND);
 
+export default app.handle;
+
 console.log(`Grafana API Server running on port ${PORT}`);
 await app.listen({ port: PORT });
