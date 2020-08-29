d156a343ef53f10a7ed85d88a6870a96469b893f
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2020-08-29 22:19:51 +0100

    vercel fixes

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,8 @@ import { Application } from "./../deps.ts";
 import { setupResponseHeaders, NOTFOUND } from "./../middleware/index.ts";
 import { router } from "./../routes/router.ts";
 
-const app = new Application();
+export const app = new Application();
+
 app.use(setupResponseHeaders);
 app.use(router.routes());
 app.use(router.allowedMethods());
--- mod.ts
+++ mod.ts
@@ -1,15 +1,5 @@
-import { Application } from "./deps.ts";
 import { PORT } from "./config/config.ts";
-import { setupResponseHeaders, NOTFOUND } from "./middleware/index.ts";
-import { router } from "./routes/router.ts";
-
-const app = new Application();
-app.use(setupResponseHeaders);
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.use(NOTFOUND);
-
-export default app.handle;
+import { app } from "./api/index.ts";
 
 console.log(`Grafana API Server running on port ${PORT}`);
 await app.listen({ port: PORT });
