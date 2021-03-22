da7f9c71d47bbfe7ff16a540efdfcb751be811e2
Sriram <yesoreyeram@gmail.com>
2021-03-22 10:05:40 +0000

    Updated std library

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-export { parse } from "https://deno.land/std@0.67.0/flags/mod.ts";
+export { parse } from "https://deno.land/std@0.91.0/flags/mod.ts";
 export {
   Application,
   Router,
   Context,
 } from "https://deno.land/x/oak@v6.0.2/mod.ts";
-export { parse as CSVParse } from "https://deno.land/std@0.67.0/encoding/csv.ts";
+export { parse as CSVParse } from "https://deno.land/std@0.91.0/encoding/csv.ts";
 export * as math from "https://cdn.skypack.dev/mathjs@v7.2.0";
--- test_deps.ts
+++ test_deps.ts
@@ -1,1 +1,1 @@
-export { assertEquals } from "https://deno.land/std@0.67.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.91.0/testing/asserts.ts";
