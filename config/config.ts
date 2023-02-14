bf39d221401a4167833e9272782fb974e265396d
Sriram <153843+yesoreyeram@users.noreply.github.com>
2023-02-14 20:33:25 +0000

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -6,6 +6,4 @@ export const PORT = args.port || 8080;
 
 export const NAMESPACE = `GrafanaUtils`;
 
-export const BANNER = `Simple Grafana Deno REST API Server.
-
-Refer https://github.com/yesoreyeram/grafana-utils for more details.`;
+export const BANNER = `Simple Grafana Deno REST API Server.`;
